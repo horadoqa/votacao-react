e14865d79b5a80afae8ae867d92a6d44c6262510
Ricardo Fahham <rfahham@hotmail.com>
2024-12-12 18:06:13 -0300

    corrigindo teste

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -1,15 +1,27 @@
 const request = require('supertest');
-const app = require('../server'); // Supondo que você tenha um servidor no arquivo 'server.js'
+const app = require('../server'); // O arquivo onde você exporta o app do Express
+
+beforeAll(() => {
+  // Iniciar o servidor antes dos testes
+  server = app.listen(5000, () => {
+    console.log('Servidor rodando na porta 5000');
+  });
+});
+
+afterAll(() => {
+  // Fechar o servidor após os testes
+  server.close();
+});
 
 describe('GET /resultados', () => {
   it('should return the correct results', async () => {
     const response = await request(app).get('/resultados');
-    expect(response.status).toBe(200);  // Verifica se a resposta tem status 200
-    expect(response.body).toEqual(expect.arrayContaining([
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(expect.arrayContaining([ // Verifique se o corpo da resposta é um array contendo objetos com os resultados esperados
       expect.objectContaining({
         framework: expect.any(String),
-        votos: expect.any(Number)
+        votos: expect.any(Number),
       })
-    ]));  // Verifica se o corpo da resposta tem o formato esperado
+    ]));
   });
 });
